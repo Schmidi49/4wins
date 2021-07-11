5174c8009ec63a034fc52c1415e05d3324897be5
Erik Schmidthaler <40146720160399@litec.ac.at>
2021-07-11 23:44:47 +0200

    added comments and changed the order of the functions to be more logical

--- game.py
+++ game.py
@@ -1,21 +1,54 @@
 import numpy as np
 
+'''
+for every gamestate of a game, the number of coulombs, rows and the gamemode must be the same
+PVE safe which player is the computer, either 1 or 2
+if PVE is 0, both players are human (default)
+'''
 ROWS = 6
 COLS = 7
 PVE = 0
 
-def setup(rows = 6, cols = 7, pve = 0):
+
+# setups the program to either start a game or include the engine
+def setup(rows=6, cols=7, pve=0):
     global ROWS, COLS, PVE
     ROWS = rows
     COLS = cols
     PVE = pve
 
 
-class state:
+'''
+each state is a unique gamestate on a board
+they can be different in playing field, in the move list etc.
+'''
+
+
+class State:
+    # the field contains each square of the field
+    # 0 are empty squares, 1 and -1 represent the stones of each player
     field = np.ndarray(shape=(COLS, ROWS), dtype=int)
+    # the result is either false if the game is in progress, 0 for draw or 1/-1 which signals a win
     result = False
+    # contains all executed moves of the gamestate
     moveList = []
+    # signals which player is currently on turn (a player is on turn until the move of the next one is executed!)
     onTurn = -1
+
+    # if a new instance of the class is created, all variables are cleared
+    def __init__(self):
+        self.field.resize(COLS, ROWS, refcheck=False)
+        self.field.fill(0)
+        self.result = False
+        self.moveList.clear
+        onTurn = -1
+
+    '''
+    calls all individual check function
+    (all of them search for a row of 4 in a certain direction)
+    additionally it checks if all squares are used and the game is a draw
+    the position of the last move must be in the argument (Format: [col,row]) to lower the calculation time
+    '''
 
     def check(self, pos):
         if len(self.moveList) == ROWS * COLS:
@@ -25,19 +58,32 @@ class state:
             return True
         elif self.checkVer(pos[0]):
             return True
-        elif self.checkDiaL(pos[1],pos[0]):
+        elif self.checkDiaL(pos[1], pos[0]):
             return True
-        elif self.checkDiaR(pos[1],pos[0]):
+        elif self.checkDiaR(pos[1], pos[0]):
             return True
         return False
 
-    def __init__(self):
-        self.field.resize(COLS, ROWS, refcheck=False)
-        self.field.fill(0)
-        self.result = False
-        self.moveList.clear
-        onTurn = -1
+    # adds a move to the gamestate and returns the exact used square of the move
+    def move(self, col):
+        col %= COLS
+        for i in range(ROWS):
+            if self.field[col][i] == 0:
+                self.field[col][i] = -self.onTurn
+                self.moveList.append(col)
+                self.onTurn = -self.onTurn
+                return [col, i]
+        return False
 
+    # just gets the position of a possible move without executing it
+    def getMove(self, col):
+        col %= COLS
+        for i in range(ROWS):
+            if self.field[col][i] == 0:
+                return [col, i]
+        return False
+
+    # executes multiple moves at once
     def executeGame(self, multiMoves):
         for singleMove in multiMoves:
             print(singleMove)
@@ -50,56 +96,54 @@ class state:
                 if self.check(turn):
                     break
 
-    def move(self, col):
-        col %= COLS
-        for i in range(ROWS):
-            if self.field[col][i] == 0:
-                self.field[col][i] = -self.onTurn
-                self.moveList.append(col)
-                self.onTurn = -self.onTurn
-                return [col, i]
-        return False
-
+    # deletes the last move
     def back(self):
         try:
-            for i in range(ROWS-1,0):
-                if self.field[self.moveList[-1]][i] is not 0:
+            for i in range(ROWS - 1, 0):
+                if self.field[self.moveList[-1]][i] != 0:
                     self.field[self.moveList[-1]][i] = 0
                     self.moveList.pop(-1)
-                    self.onTurn=-self.onTurn
+                    self.onTurn = -self.onTurn
                     return
         except LookupError:
-            print("No removeable error")
+            print("No removable error")
 
-
-    def getMove(self, col):
-        col %= COLS
-        for i in range(ROWS):
-            if self.field[col][i] == 0:
-                return [col, i]
-        return False
-
+    # generates a list of all currently possible moves by looking into the top-square
     def genMoves(self):
         moves = []
         for i in range(COLS):
-            if self.field[i][ROWS-1] == 0:
+            if self.field[i][ROWS - 1] == 0:
                 moves.append(i)
 
+    # checks for a horizontal line of 4 (only the row of the executed move)
     def checkHor(self, row):
         count = 0
+        # check every square in the row
         for i in range(COLS):
+            '''
+            if the square belongs the current player on turn, count up
+            if the counter reaches 4 -> player won
+            if there is an enemy or empty field in between -> reset the counter
+            '''
             if self.field[i][row] == self.onTurn:
-                count +=1
+                count += 1
                 if count == 4:
                     self.result = self.onTurn
                     return self.onTurn
             else:
-                count=0
+                count = 0
         return False
 
+    # checks for a vertical line of 4 (only the coulomb of the executed move)
     def checkVer(self, col):
         count = 0
+        # check every square of the coulomb
         for i in range(ROWS):
+            '''
+            if the square belongs the current player on turn, count up
+            if the counter reaches 4 -> player won
+            if there is an enemy or empty field in between -> reset the counter
+            '''
             if self.field[col][i] == self.onTurn:
                 count += 1
                 if count == 4:
@@ -109,13 +153,20 @@ class state:
                 count = 0
         return False
 
+    # checks for a diagonal line of 4 (only the diagonal to the down right which contains the current move)
     def checkDiaR(self, row, col):
         count = 0
+        # checks if the start square of the diagonal is ont vertical or horizontal border
         if col > (ROWS - 1 - row):
             start = ROWS - 1 - row
         else:
             start = col
         i = 0
+        '''
+        from then on go threw each square of the diagonal
+        if the counter reaches 4 -> player won
+        if there is an enemy or empty field in between -> reset the counter
+        '''
         while (row + start - i) > -1 and (col - start + i) < COLS:
             if self.field[col - start + i][row + start - i] == self.onTurn:
                 count += 1
@@ -124,16 +175,23 @@ class state:
                     return self.onTurn
             else:
                 count = 0
-            i+=1
+            i += 1
         return False
 
+    # checks for a diagonal line of 4 (only the diagonal to the down left which contains the current move)
     def checkDiaL(self, row, col):
         count = 0
+        # checks if the start square of the diagonal is ont vertical or horizontal border
         if (ROWS - 1 - row) > (COLS - 1 - col):
             start = COLS - 1 - col
         else:
             start = ROWS - 1 - row
         i = 0
+        '''
+        from then on go threw each square of the diagonal
+        if the counter reaches 4 -> player won
+        if there is an enemy or empty field in between -> reset the counter
+        '''
         while (row + start - i) > -1 and (col + start - i) > -1:
             if self.field[col + start - i][row + start - i] == self.onTurn:
                 count += 1
--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import sys
 import prnt
 import game
+
+
 # import evaluation as ev
 
 
 def ui(rows=6, cols=7, pve=0):
+    # converts arguments into usable data
     rows = int(rows)
     cols = int(cols)
     if not ((pve == 0) | (pve == 1) | (pve == 2)):
         pve = 0
     game.setup(rows, cols, pve)
-    curGame = game.state()
 
+    # setup a gamestate, which is the current game
+    curGame = game.State()
+
+    '''
+    turn is the returned value of the check-function
+    if the return is false, the gamestate isn't over
+    if the value is true, there is a result (win, draw or lose)
+    '''
     turn = False
+
     while True:
+        # prints the board
         prnt.board(curGame.field)
+        # only except ints as valid inputs
+
         try:
             turn = curGame.move(int(input("Zug: ")))
         except ValueError:
             print("Input can only be an integer!")
+
+        # if there is an result, inform the user
         if turn:
             if curGame.check(turn):
                 prnt.board(curGame.field)
@@ -27,8 +43,9 @@ def ui(rows=6, cols=7, pve=0):
                 elif curGame.result == -1:
                     print("Player 2 won!")
                 elif curGame.result == 0:
-                    print("curGame ended in Draw")
+                    print("Game ended in Draw")
                 break
+    # at the end of the game, print the list of executed moves
     print(curGame.moveList)
 
 
